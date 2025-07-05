d7dd8c5f0b278f0ad0fe0fbb7c91a8f7d2458859
fer21047 <yvmgfer@gmail.com>
2025-07-05 16:02:00 -0600

    Se agrega este commit 1

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/desafio5/',
   plugins: [react()],
   server: {
     port: Number(process.env.PORT) || 3000, // Usa variable de entorno o 3000 por defecto
